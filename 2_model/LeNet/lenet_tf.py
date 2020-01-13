763150212c3c683dc9aa813b751d7c1f49d73cc6
nikingo <tsu.kamo.sub@gmail.com>
2020-01-13 22:01:23 +0900

    add test session for lenet_tf, and add graph of LeNet

--- 2_model/LeNet/lenet_tf.py
+++ 2_model/LeNet/lenet_tf.py
@@ -34,13 +34,13 @@ print(data_path)
 train_tensor = torchvision.datasets.MNIST(data_path, train=True, download=True, transform=torchvision.transforms.ToTensor())
 test_tensor = torchvision.datasets.MNIST(data_path, train=False, download=True, transform=torchvision.transforms.ToTensor())
 
-train_data = train_tensor.data.cpu().numpy()[:10000]
+train_data = train_tensor.data.cpu().numpy()[:20000]
 print(type(train_data), train_data.shape)
-train_label = train_tensor.targets.cpu().numpy()[:10000]
+train_label = train_tensor.targets.cpu().numpy()[:20000]
 print(type(train_label), train_label.shape)
-test_data = test_tensor.data.cpu().numpy()[:1000]
+test_data = test_tensor.data.cpu().numpy()[:2000]
 print(type(test_data), test_data.shape)
-test_label = test_tensor.targets.cpu().numpy()[:1000]
+test_label = test_tensor.targets.cpu().numpy()[:2000]
 print(type(test_label), test_label.shape)
 
 
@@ -78,6 +78,54 @@ def Mynet(x):
     return x
 
 
+def LeNet(x):
+
+    x = tf.pad(x, tf.constant([[0, 0,], [2, 2], [2, 2], [0, 0]]), "CONSTANT")
+    print(x.shape)
+    w = tf.Variable(tf.random_normal([5, 5, 1, 6]), name='w1')  #重みの初期値の変数を定義
+    x = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='VALID')
+    b = tf.Variable(tf.random_normal([6]), name='b1')
+    x = tf.nn.bias_add(x, b)
+    x = tf.nn.sigmoid(x)
+    print(x.shape)
+    x = tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    print(x.shape)
+
+    w = tf.Variable(tf.random_normal([5, 5, 6, 16]), name='w2')  #重みの初期値の変数を定義
+    x = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='VALID')
+    b = tf.Variable(tf.random_normal([16]), name='b2')
+    x = tf.nn.bias_add(x, b)
+    x = tf.nn.sigmoid(x)
+    print(x.shape)
+    x = tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    print(x.shape)
+
+    mb, h, w, c = x.get_shape().as_list()   #ネットワークの返り値のshapeを取得
+    x = tf.reshape(x, [-1, h*w*c])  #画像・チャンネルを一列のデータに均す
+    w = tf.Variable(tf.random_normal([w*h*c, 120]), name='w3')
+    x = tf.matmul(x, w) #行列積
+    b = tf.Variable(tf.random_normal([120]), name='b3')
+    x = tf.add(x, b)    #1-Dの加算
+    print(x.shape)
+
+    w = tf.Variable(tf.random_normal([120, 64]), name='w4')
+    x = tf.matmul(x, w) #行列積
+    b = tf.Variable(tf.random_normal([64]), name='b4')
+    x = tf.add(x, b)    #1-Dの加算
+    print(x.shape)
+
+    w = tf.Variable(tf.random_normal([64, num_classes]), name='w5')
+    x = tf.matmul(x, w) #行列積
+    b = tf.Variable(tf.random_normal([num_classes]), name='b5')
+    x = tf.add(x, b)    #1-Dの加算
+    print(x.shape)
+
+    #x = tf.nn.relu(x)
+    #print(x.shape)
+
+    return x
+
+
 def train_net(train, preds, accuracy, loss, merged, xs, ys):
     config = tf.ConfigProto()
     sess = tf.InteractiveSession(config=config)
@@ -107,12 +155,32 @@ def train_net(train, preds, accuracy, loss, merged, xs, ys):
     #summary_writer.close()
 
 
+def test_net(xs, ys):
+
+    config = tf.ConfigProto()
+    with tf.Session(config=config) as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, os.path.join(base_path, "tf_cnn.ckpt"))
+
+        # for i in range(len(ys)):
+        #     input_shape = xs[i].shape
+        #     x = xs[i].reshape(1, input_shape[0], input_shape[1],input_shape[2]) #(?, height, width, channel) の形に合わせて、サイズ1のバッチにする
+        #     y = ys[i]
+
+        #     pred = sess.run([out], feed_dict={X:x})[0]
+        #     print('label:', y, 'out:', pred)
+        
+        acc = sess.run([accuracy], feed_dict={X:xs, Y:ys})
+        print('accuracy >>', acc)
+
+
 #train
 X = tf.placeholder(tf.float32, [None, img_height, img_width, 1])    #入力のプレースホルダーを定義(noneはなんでもいい値)
 Y = tf.placeholder(tf.float32, [None, num_classes]) #ラベルのプレースホルダーを定義
 
 # 自分でlayerを定義した時
-logits = Mynet(X)
+#logits = Mynet(X)
+logits = LeNet(X)
 
 preds = tf.nn.softmax(logits)   #Mynetの結果をsoftmaxする計算の定義
 loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(logits=logits, onehot_labels=Y))  #Mynetの結果とラベルとの間で誤差関数の計算をする計算の定義
@@ -130,5 +198,23 @@ merged = tf.summary.merge_all()
 
 xs = train_data.reshape(-1, img_height, img_width, 1)
 ys = np.identity(num_classes)[train_label]
-print(ys)
+#print(ys)
 train_net(train, correct_pred, accuracy, loss, merged, xs, ys)    #定義した計算を渡して学習sessionを実行させる
+
+
+#test
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, img_height, img_width, 1])
+Y = tf.placeholder(tf.float32, [None, num_classes])
+
+#logits = Mynet(X)
+logits = LeNet(X)
+out = tf.nn.softmax(logits)
+correct_pred = tf.equal(tf.argmax(out, 1), tf.argmax(Y, 1))   #
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))    #
+
+xs = test_data.reshape(-1, img_height, img_width, 1)
+ys = np.identity(num_classes)[test_label]
+#print(ys)
+test_net(xs, ys)
