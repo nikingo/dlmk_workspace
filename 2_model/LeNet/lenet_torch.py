3ffe43b8cf825747ef3a30923fa29ca6a04f19d9
nikingo <tsu.kamo.sub@gmail.com>
2020-02-03 23:07:54 +0900

    add LeNet on pytorch

--- 2_model/LeNet/lenet_torch.py
+++ 2_model/LeNet/lenet_torch.py
@@ -44,49 +44,37 @@ test_label = test_tensor.targets.cpu()[:2000]
 print(type(test_label), test_label.size())
 
 
-class Mynet(torch.nn.Module):
+class LeNet(torch.nn.Module):
     def __init__(self):
-        super(Mynet, self).__init__()
+        super(LeNet, self).__init__()
 
-        self.conv1_1 = torch.nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.bn1_1 = torch.nn.BatchNorm2d(32)
-        self.conv1_2 = torch.nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.bn1_2 = torch.nn.BatchNorm2d(32)
-        self.conv2_1 = torch.nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn2_1 = torch.nn.BatchNorm2d(64)
-        self.conv2_2 = torch.nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn2_2 = torch.nn.BatchNorm2d(64)
-        self.conv3_1 = torch.nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn3_1 = torch.nn.BatchNorm2d(128)
-        self.conv3_2 = torch.nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn3_2 = torch.nn.BatchNorm2d(128)
-        self.conv4_1 = torch.nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.bn4_1 = torch.nn.BatchNorm2d(256)
-        self.conv4_2 = torch.nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.bn4_2 = torch.nn.BatchNorm2d(256)
-        self.fc1 = torch.nn.Linear(img_height//16 * img_width//16 * 256, 512)
-        #self.fc1_d = torch.nn.Dropout2d()
-        self.fc2 = torch.nn.Linear(512, 512)
-        self.fc_out = torch.nn.Linear(512, num_classes)
+        self.conv1 = torch.nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0)
+
+        self.conv2 = torch.nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0)
+
+        input_size = np.array([img_height, img_width])
+        self.fc1_input_size = ((((input_size - 5 + 1) / 2) - 5 + 1) / 2).astype(np.int32)   #全結合層のために、入力サイズ数を画像サイズ*チャンネル数で計算
+
+        self.fc1 = torch.nn.Linear(self.fc1_input_size[0] * self.fc1_input_size[1] * 16, 120)
+
+        self.fc2 = torch.nn.Linear(120, 64)
+
+        self.fc_out = torch.nn.Linear(64, num_classes)
 
     def forward(self, x):
-        x = F.relu(self.bn1_1(self.conv1_1(x)))
-        x = F.relu(self.bn1_2(self.conv1_2(x)))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.bn2_1(self.conv2_1(x)))
-        x = F.relu(self.bn2_2(self.conv2_2(x)))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.bn3_1(self.conv3_1(x)))
-        x = F.relu(self.bn3_2(self.conv3_2(x)))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.bn4_1(self.conv4_1(x)))
-        x = F.relu(self.bn4_2(self.conv4_2(x)))
-        x = F.max_pool2d(x, 2)
-        x = x.view(-1, img_height//16 * img_width//16 * 256)
-        x = F.relu(self.fc1(x))
-        #x = self.fc1_d(x)
-        x = F.relu(self.fc2(x))
+        x = F.pad(x, (2, 2, 2, 2))  #sizeを28->32にするためのpadding
+
+        x = self.conv1(x)
+        x = torch.sigmoid(F.max_pool2d(x, 2))
+        
+        x = self.conv2(x)
+        x = torch.sigmoid(F.max_pool2d(x, 2))
+
+        x = x.view(-1, self.fc1_input_size[0] * self.fc1_input_size[1] * 16)
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc_out(x)
+
         return x
 
 
@@ -127,7 +115,7 @@ def train_net(model, opt, xs, ys):
 
     #xs, ys = data_load(data_path, 64, 64, hflip=True, vflip=True, rot=[angle for angle in range(0,360,10)])
 
-    ind_batch = get_shuffled_batch_ind(len(ys), 1024, 10)
+    ind_batch = get_shuffled_batch_ind(len(ys), 512, 20)
     iter_per_epoch = len(ys) // 512
 
     running_loss = 0
@@ -182,13 +170,13 @@ def test_net(model, xs, ys):
     print('Accuracy %d / %d = %f' % (correct, total, correct / total))
 
 
-#model = Mynet()
-model = Simplenet()
+model = LeNet()
+#model = Simplenet()
 model.to(device)
 
 opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-#train_net(model, opt, train_data, train_label)
-#torch.save(model.state_dict(), os.path.join(base_path, "cnn.pt"))
-model.load_state_dict(torch.load(os.path.join(base_path, "cnn.pt")))
+train_net(model, opt, train_data, train_label)
+torch.save(model.state_dict(), os.path.join(base_path, "cnn_LeNet.pt"))
+model.load_state_dict(torch.load(os.path.join(base_path, "cnn_LeNet.pt")))
 test_net(model, test_data, test_label)
